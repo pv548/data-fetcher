599caf4ba407cad5a80970943fae175f031a0cc1
Petr Vasilyev <petr.vasilyev@stud.uni-bamberg.de>
2023-03-25 15:45:45 +0100

    add todo and fix window size

--- build.js
+++ build.js
@@ -7,7 +7,7 @@ const screenshot = 'spond.png';
     const loginPage = 'https://spond.com/landing/login/';
     const clientPage = 'https://spond.com/landing/login/';
 
-    const browser = await puppeteer.launch({ headless: true })
+    const browser = await puppeteer.launch({ headless: true, defaultViewport: {width:1920,height:1080} })
     const page = await browser.newPage()
     await page.goto(loginPage, {waitUntil: 'networkidle2'});
     await page.type('[name="emailOrPhoneNumber"]', process.env.SPOND_USER)
@@ -19,10 +19,9 @@ const screenshot = 'spond.png';
     await page.screenshot({ path: screenshot })
     
     browser.close()
-    
     console.log('See screenshot: ' + screenshot)
    
+    // TODO: fetch event data from DOM
     //var linkTexts = await page.$$eval("div.spondListstyled__SpondItem", elements => elements.map(item=>item.textContent))
     //await console.log(linkTexts)
-
 })()
